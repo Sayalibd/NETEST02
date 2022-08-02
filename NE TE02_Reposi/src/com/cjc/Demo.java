b11fbabbf6b27f814eb09953b70e11d811ca1eb4
Sayalibd <110378716+Sayalibd@users.noreply.github.com>
2022-08-02 14:18:19 +0530

    By git

--- NE TE02_Reposi/src/com/cjc/Demo.java
+++ NE TE02_Reposi/src/com/cjc/Demo.java
@@ -4,7 +4,11 @@ public class Demo {
 	
 	public void m1()
 	{
-	  System.out.println("M1---Method);
+	  System.out.println("M1---Method");
+	}
+	public void m2()
+	{
+	  System.out.println("M2---Method");
 	}
 
 	public static void main(String[] args) 
