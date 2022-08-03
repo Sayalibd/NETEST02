3d993021ce21feacac723c9e6f935f36f4f1a3a5
Sayalibd <110378716+Sayalibd@users.noreply.github.com>
2022-08-03 10:39:25 +0530

    By sbd1

--- NE TE02_Reposi/src/com/cjc/Demo2.java
+++ NE TE02_Reposi/src/com/cjc/Demo2.java
@@ -2,6 +2,17 @@ package com.cjc;
 
 public class Demo2 {
 
+	public void n1()
+	{
+	  System.out.println("n1---method");
+	}
+	
+	public void n2()
+	{
+	  System.out.println("n2---method");
+	}
+	
+	
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello");
